68b471c15d3cd5ef2f0047b8e31be32a99e6b14f
Julien Crutain <juju510@hotmail.fr>
2022-07-01 11:05:26 +0200

    reset front for v2

--- front/src/App.js
+++ front/src/App.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import classes from "./app.module.css";
-import {Routes, Route} from "react-router-dom";
-import Home from "./containers/pages/home/home";
-import FormAuth from "./containers/pages/authentification/formAuth";
-import Erreur from './containers/pages/pageErreur/erreur';
-import Erreur404 from './components/erreur/erreur404';
-import HomePost from './containers/pages/homePost/homePost';
-import PageProfils from './containers/pages/pageProfils/pageProfils';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <main className={classes.main}>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-          <Route path="signup" element={<FormAuth mode="signup" />}/>
-          <Route path="login" element={<FormAuth mode="login"/>}/>
-          <Route path="groupomania" element={<HomePost />}/>
-          <Route path="profils" element={<PageProfils />} />
-        <Route/>
-        <Route path="*" element={<Erreur><Erreur404/></Erreur>}/>
-
-      </Routes>
-    </main>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- front/src/components/nav/auth/navAuth.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-
-const NavAuth = (props) => (
-            <NavLink to={props.to} className={props.cssLi}>{props.children}</NavLink>
-);
-
-export default NavAuth;
